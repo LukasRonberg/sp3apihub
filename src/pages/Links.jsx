85c8771e3b1925d6749a0d451e68927af4c360d9
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2024-12-08 11:25:49 +0100

    Added content to Links page

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,7 +1,11 @@
 function Links() {
     return (  
         <>
-        <h1>Links</h1>
+            <h1>Links</h1>
+            <ul>
+                <li><a href="https://github.com/LukasRonberg/SwipeyBoi">GitHub Repository for Backend</a></li>
+                <li><a href="https://hotel.lukasronberg.dk/api/routes">Backend API</a></li>
+            </ul>
         </>
     );
 }
